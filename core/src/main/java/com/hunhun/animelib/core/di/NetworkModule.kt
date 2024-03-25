086f0b7a9cc50340ee755487b52ff41296cc9ed2
hadyandh <hadyandh@gmail.com>
2024-03-25 14:59:09 +0800

    feat: certificate pinning

--- core/src/main/java/com/hunhun/animelib/core/di/NetworkModule.kt
+++ core/src/main/java/com/hunhun/animelib/core/di/NetworkModule.kt
@@ -5,6 +5,7 @@ import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -14,12 +15,18 @@ import java.util.concurrent.TimeUnit
 @Module
 @InstallIn(SingletonComponent::class)
 class NetworkModule {
+    val hostname = "api.jikan.moe"
+    val certificatePinner = CertificatePinner.Builder()
+        .add(hostname, "sha256/X0OZQDQNK3UAKbtvr/zWMc/RYdPVrs+xSh8c80dqpTI=")
+        .build()
+
     @Provides
     fun provideOkHttpClient(): OkHttpClient {
         return OkHttpClient.Builder()
             .addInterceptor(HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY))
             .connectTimeout(120, TimeUnit.SECONDS)
             .readTimeout(120, TimeUnit.SECONDS)
+            .certificatePinner(certificatePinner)
             .build()
     }
 
