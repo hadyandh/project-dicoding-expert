4231a246ffeae54aea49a20227e79a4338a471d3
hadyandh <hadyandh@gmail.com>
2024-03-25 16:20:09 +0800

    fix: inspect code performance

--- core/src/main/java/com/hunhun/animelib/core/ui/AnimeAdapter.kt
+++ core/src/main/java/com/hunhun/animelib/core/ui/AnimeAdapter.kt
@@ -3,6 +3,8 @@ package com.hunhun.animelib.core.ui
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.hunhun.animelib.core.R
@@ -14,17 +16,30 @@ class AnimeAdapter : RecyclerView.Adapter<AnimeAdapter.ListViewHolder>() {
     private var listData = ArrayList<Anime>()
     var onItemClick: ((Anime) -> Unit)? = null
 
+    private val diffUtil = object : DiffUtil.ItemCallback<Anime>() {
+        override fun areItemsTheSame(oldItem: Anime, newItem: Anime): Boolean {
+            return oldItem.id == newItem.id
+        }
+
+        override fun areContentsTheSame(oldItem: Anime, newItem: Anime): Boolean {
+            return oldItem == newItem
+        }
+
+    }
+
+    private val asyncListDiffer = AsyncListDiffer(this, diffUtil)
+
     fun setData(newListData: List<Anime>?) {
         if (newListData == null) return
         listData.clear()
         listData.addAll(newListData)
-        notifyDataSetChanged()
+        asyncListDiffer.submitList(listData)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
         ListViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.row_item_anime, parent, false))
 
-    override fun getItemCount() = listData.size
+    override fun getItemCount() = asyncListDiffer.currentList.size
 
     override fun onBindViewHolder(holder: ListViewHolder, position: Int) {
         val data = listData[position]
